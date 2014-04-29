ae9b02a5c39492f160ccdb41098117af6235cf05
MrD <the.ubik@gmail.com>
2014-04-29 18:04:17 +0100

    [NOOP]disk -> file

--- src/gr/uoa/di/mde515/engine/buffer/HF.java
+++ src/gr/uoa/di/mde515/engine/buffer/HF.java
@@ -2,12 +2,15 @@ package gr.uoa.di.mde515.engine.buffer;
 
 import gr.uoa.di.mde515.index.Record;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 
 public class HF {
 
 	private BufferManager buf;
+	// storage layer
+	private final DiskFile file;
 	// useful constants
 	private static final int PAGE_SIZE = 32; // TODO move to globals
 	private static final int RECORD_SIZE = 8; // TODO read from header
@@ -27,6 +30,11 @@ public class HF {
 	private static final int OFFSET_CURRENT_NUMBER_OF_SLOTS = 8;
 
 	public HF() {
+		try {
+			file = new DiskFile("test.db");
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException("Can't access db file", e);
+		}
 		buf = BufferManager.getInstance();
 	}
 
@@ -54,15 +62,15 @@ public class HF {
 	 * @throws IOException
 	 */
 	public void createFileHeader() throws IOException {
-		buf.newPage(0);
-		Frame f = buf.allocFrame(0);
+		file.allocateNewPage(0);
+		Frame f = buf.allocFrame(0, file);
 		Page p = new Page(0, f.getBufferFromFrame());
 		p.writeInt(OFFSET_PAGES_COUNT, 0);
 		// p.writeInt(OFFSET_FREE_LIST, 1);
 		// p.writeInt(OFFSET_FULL_LIST, -1);
 		p.writeShort(OFFSET_RECORD_SIZE, (short) RECORD_SIZE);
 		f.setDirty();
-		buf.flushFileHeader();
+		buf.flushFileHeader(file);
 	}
 
 	/**
@@ -72,13 +80,13 @@ public class HF {
 	 * @throws IOException
 	 */
 	public void createPageHeader(int pageID) throws IOException {
-		buf.newPage(pageID);
+		file.allocateNewPage(pageID);
 		System.out.println("---Inside the createPageHeader---");
 		// System.out
 		// .println("The last allocated pageID after newPage execution is "
 		// + DiskManager.last_allocated_pageID);
 		System.out.println("---Inside the createPageHeader---");
-		Frame f = buf.allocFrame(pageID);
+		Frame f = buf.allocFrame(pageID, file);
 		Page p = new Page(pageID, f.getBufferFromFrame());
 		p.writeInt(OFFSET_CURRENT_PAGE, pageID);
 		// p.writeInt(NEXT_PAGE_OFFSET, (DiskManager.last_allocated_pageID));
@@ -90,7 +98,7 @@ public class HF {
 		// garbage collect page object
 		p = null;
 		try {
-			buf.flushPage(pageID);
+			buf.flushPage(pageID, file);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -118,7 +126,7 @@ public class HF {
 		boolean status = true;
 		int key = (Integer) record.getKey();
 		int value = (Integer) record.getValue();
-		Frame f = buf.allocFrame(0);
+		Frame f = buf.allocFrame(0, file);
 		Page header = new Page(0, f.getBufferFromFrame());
 		pageCount = header.readInt(OFFSET_PAGES_COUNT);
 		System.out.println("THE PAGE COUNT IS " + pageCount);
@@ -144,12 +152,12 @@ public class HF {
 			System.out.println("THE POSITION IS "
 				+ (((pageCount) * ENTRY_SIZE) + FILEHEADER_LENGTH));
 			// this might be outside
-			buf.flushFileHeader();
+			buf.flushFileHeader(file);
 		}
 		// changing the pageID so that it follows the pageID=0 in loop is page 1
 		pageID = pageID + 1;
 		System.out.println("THE PAGEID IS before Frame in " + pageID);
-		Frame in = buf.allocFrame(pageID);
+		Frame in = buf.allocFrame(pageID, file);
 		Page p = new Page(pageID, in.getBufferFromFrame());
 		int freeSlot = p.readInt(OFFSET_NEXT_FREE_SLOT);
 		int current_number_of_slots = p.readInt(OFFSET_CURRENT_NUMBER_OF_SLOTS);
@@ -173,7 +181,7 @@ public class HF {
 			System.out.println("The curr_id is " + curr_id);
 			header.writeInt((curr_id * ENTRY_SIZE) + FILEHEADER_LENGTH, -1);
 			// this might be outside
-			buf.flushFileHeader();
+			buf.flushFileHeader(file);
 		}
 		// sets the frame dirty
 		in.setDirty();
@@ -181,7 +189,7 @@ public class HF {
 		header = null;
 		p = null;
 		try {
-			buf.flushPage(pageID);
+			buf.flushPage(pageID, file);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
