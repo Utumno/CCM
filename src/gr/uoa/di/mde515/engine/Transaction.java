de9d9cc78d54bd032416c261f1512e8eaf02e123
MrD <the.ubik@gmail.com>
2014-05-14 19:36:50 +0100

    Transaction: disallow up/down grading locks

--- src/gr/uoa/di/mde515/engine/Transaction.java
+++ src/gr/uoa/di/mde515/engine/Transaction.java
@@ -7,16 +7,19 @@ import gr.uoa.di.mde515.locks.LockManager;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class Transaction {
 
 	private static final AtomicLong transactionId;
 	private static final LockManager lm = LockManager.getInstance();
-	private final long threadId; // not really needed
+	private final long threadId; // not really needed FIXME
 	private final String threadName;
-	private final List<PageId<Integer>> lockedDataPages = new ArrayList<>();
+	private final EnumMap<DBLock, List<PageId<Integer>>> lockedDataPages = new EnumMap<>(
+		DBLock.class);
 	// TODO Random unique trans identifier added to thread name
 	// http://www.javapractices.com/topic/TopicAction.do?Id=56
 	// http://bugs.java.com/view_bug.do?bug_id=6611830
@@ -30,6 +33,8 @@ public class Transaction {
 		threadId = Thread.currentThread().getId();
 		threadName = "Thread (" + threadId + ") for transaction " + transId;
 		Thread.currentThread().setName(threadName);
+		lockedDataPages.put(DBLock.E, new ArrayList<PageId<Integer>>());
+		lockedDataPages.put(DBLock.S, new ArrayList<PageId<Integer>>());
 	}
 
 	public void validateThread() {
@@ -43,19 +48,21 @@ public class Transaction {
 
 	public <K extends Comparable<K>, V> void
 			flush(final DataFile<K, V> dataFile) throws IOException {
-		dataFile.flush(lockedDataPages);
+		for (List<PageId<Integer>> list : lockedDataPages.values())
+			dataFile.flush(list);
 	}
 
 	public <K extends Comparable<K>, V> void
 			abort(final DataFile<K, V> dataFile) throws IOException {
-		dataFile.abort(lockedDataPages);
+		for (List<PageId<Integer>> list : lockedDataPages.values())
+			dataFile.abort(list);
 	}
 
-	public void addLockedDataPage(PageId<Integer> pageId) {
+	public void addLockedDataPage(PageId<Integer> pageId, DBLock lock) {
 		if (!(pageId.getId() instanceof Integer)) return; // FIXME horrible hack
 		// - actually the pageId may wrap a node at this stage
 		System.out.println("ADDED " + pageId.getId());
-		lockedDataPages.add(pageId);
+		lockedDataPages.get(lock).add(pageId);
 	}
 
 	/**
@@ -71,9 +78,16 @@ public class Transaction {
 	 */
 	public boolean lock(int pageID, DBLock lock) {
 		final PageId<Integer> pid = new PageId<>(pageID);
-		if (!lockedDataPages.contains(pid)) {
+		for (Entry<DBLock, List<PageId<Integer>>> entries : lockedDataPages
+			.entrySet()) {
+			if (entries.getKey() != lock && entries.getValue().contains(pid))
+				throw new IllegalStateException("You already hold a "
+					+ entries.getKey() + " lock for page " + pageID
+					+ ". You can't acquire a " + lock + " lock.");
+		}
+		if (!lockedDataPages.get(lock).contains(pid)) {
 			lm.requestLock(new LockManager.Request(pid, this, lock));
-			addLockedDataPage(pid);
+			addLockedDataPage(pid, lock);
 			return true;
 		}
 		return false;
