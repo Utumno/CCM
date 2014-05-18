9b2ed6e4a5b7d8cc73655b63bb46daa23886dfa8
kleom <kleomenis.tsiligkos@gmail.com>
2014-05-18 22:09:12 +0300

    graphs

---
+++ src/gr/uoa/di/mde515/locks/Graph.java
@@ -0,0 +1,74 @@
+package gr.uoa.di.mde515.locks;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.jgrapht.DirectedGraph;
+import org.jgrapht.alg.CycleDetector;
+import org.jgrapht.alg.cycle.JohnsonSimpleCycles;
+import org.jgrapht.graph.DefaultEdge;
+
+public class Graph {
+
+	public void breakCycle(DirectedGraph<String, DefaultEdge> graph) {
+		// local to the method
+		JohnsonSimpleCycles<String, DefaultEdge> j = new JohnsonSimpleCycles<>(
+			graph);// using the graph
+		// using the graph+ local to the method
+		CycleDetector<String, DefaultEdge> t = new CycleDetector<>(graph);
+		while (t.detectCycles()) {
+			// local to the method
+			List<List<String>> findS = j.findSimpleCycles();
+			System.out.println("The cycles are " + findS);
+			// local to the method
+			Map<String, Integer> map = new HashMap<>();
+			for (int i = 0; i < findS.size(); i++) {
+				for (int j1 = 0; j1 < findS.get(i).size(); j1++) {
+					String key = findS.get(i).get(j1);
+					if (map.containsKey(key)) {
+						int value = map.get(key);
+						map.put(key, value + 1);
+					} else {
+						map.put(key, 1);
+					}
+				}
+			}
+			Entry<String, Integer> entry = findMax(map);
+			String str = entry.getKey();
+			int freq = entry.getValue();
+			System.out.println("The frequency is " + freq);
+			if (freq > 1) {
+				System.out.println("VERTEX");
+				graph.removeVertex(str);// using the graph structure
+										// needs synchronization
+			} else {
+				// for (int i = 0; i < findS.size(); i++) {
+				System.out.println("The vertex to be removed is " + str);
+				graph.removeVertex(str);// using the graph structure
+										// needs synchronization
+				System.out.println("After removal");
+				System.out.println("Does it contain a cycle? "
+					+ t.detectCycles());
+				List<List<String>> qwe = j.findSimpleCycles();
+				System.out.println("The cycles are " + qwe);
+				// }
+			}
+			System.out.println("The max value is " + str);
+			System.out.println("Does it contain a cycle? " + t.detectCycles());
+		}
+		System.out.println("While loop ended");
+	}
+
+	private Entry<String, Integer> findMax(Map<String, Integer> map) {
+		Map.Entry<String, Integer> maxEntry = null;
+		for (Map.Entry<String, Integer> entry : map.entrySet()) {
+			if (maxEntry == null
+				|| entry.getValue().compareTo(maxEntry.getValue()) > 0) {
+				maxEntry = entry;
+			}
+		}
+		return maxEntry;
+	}
+}
---
+++ src/gr/uoa/di/mde515/locks/GraphMain.java
@@ -0,0 +1,59 @@
+package gr.uoa.di.mde515.locks;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.jgrapht.DirectedGraph;
+import org.jgrapht.alg.CycleDetector;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleDirectedGraph;
+
+public class GraphMain {
+
+	public static void main(String[] args) {
+		DirectedGraph<String, DefaultEdge> graph = new SimpleDirectedGraph<String, DefaultEdge>(
+			DefaultEdge.class);
+		Graph g = new Graph();
+		String G = "G";
+		String L = "L";
+		String K = "K";
+		String D = "D";
+		String M = "M";
+		// add the vertices
+		graph.addVertex(G);
+		graph.addVertex(L);
+		graph.addVertex(K);
+		graph.addVertex(D);
+		graph.addVertex(M);
+		// add edges to create a circuit
+		graph.addEdge(G, L);
+		graph.addEdge(L, G);
+		graph.addEdge(K, L);
+		graph.addEdge(L, K);
+
+		graph.addEdge(D, M);
+		graph.addEdge(M, D);
+		System.out.println(graph.toString());
+		System.out.println("Does it contain the edge? "
+			+ graph.containsEdge(G, D));
+		CycleDetector<String, DefaultEdge> t = new CycleDetector<>(graph);
+		System.out.println("Does it contain a cycle? " + t.detectCycles());
+		System.out.println("Is the M part of the cycle? "
+			+ t.detectCyclesContainingVertex(M));
+		Set<String> s = new TreeSet<String>();
+		System.out.println("The find cycles " + t.findCycles());
+		System.out.println("The cycle containing vertex M is "
+			+ t.findCyclesContainingVertex(M));
+		s = t.findCyclesContainingVertex(D);
+		Iterator<String> iter = s.iterator();
+		while (iter.hasNext()) {
+			System.out.println(iter.next() + " ");
+		}
+
+		g.breakCycle(graph);
+
+
+
+	}
+}
