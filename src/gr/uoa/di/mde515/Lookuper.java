e04b6631966f60c7b31911dee5bd6f2c9ba770bb
kleom <kleomenis.tsiligkos@gmail.com>
2014-05-19 20:39:37 +0300

    Lookuper

---
+++ src/gr/uoa/di/mde515/Lookuper.java
@@ -0,0 +1,35 @@
+package gr.uoa.di.mde515;
+
+import gr.uoa.di.mde515.engine.Engine;
+import gr.uoa.di.mde515.engine.Transaction;
+import gr.uoa.di.mde515.index.Record;
+import gr.uoa.di.mde515.locks.DBLock;
+
+import java.util.concurrent.Callable;
+
+final class Lookuper<T> implements Callable<T> {
+
+	private final Engine<Integer, Integer, Integer> eng;
+	private final Integer key;
+
+	Lookuper(Engine<Integer, Integer, Integer> eng, Integer key) {
+		this.eng = eng;
+		this.key = key;
+	}
+
+	@Override
+	public T call() throws Exception {
+		Transaction tr = eng.beginTransaction();
+		try {
+			Record<Integer, Integer> rkv = eng.lookup(tr, key, DBLock.E);
+			System.out
+				.println("The record is "
+					+ (rkv == null ? null
+							: ("key " + rkv.getKey() + "  value " + rkv
+								.getValue())));
+		} finally {
+			eng.endTransaction(tr);
+		}
+		return null;
+	}
+}
