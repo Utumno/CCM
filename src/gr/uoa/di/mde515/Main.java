4f99770a0f00413e681d5f0f049f2ec8e5d3c695
kleom <kleomenis.tsiligkos@gmail.com>
2014-05-23 08:12:05 +0300

    Main changes

--- src/gr/uoa/di/mde515/Main.java
+++ src/gr/uoa/di/mde515/Main.java
@@ -24,6 +24,7 @@ public class Main {
 
 	private static Random r = new Random();
 	// THREADS //
+	// private static final int NUM_OF_THREADS = Runtime.getRuntime()
 	private static final int NUM_OF_THREADS = 1;
 	private static final ExecutorService exec = Executors
 		.newFixedThreadPool(NUM_OF_THREADS);
@@ -32,13 +33,20 @@ public class Main {
 			IOException {
 		final Engine<Integer, Integer, Integer> eng = Engine.newInstance();
 		try {
+			// ArrayList<InsertAbort<T>> arrayList = new ArrayList<>();
 			// ArrayList<Inserter<T>> arrayList = new ArrayList<>();
 			// ArrayList<Lookuper<T>> arrayList = new ArrayList<>();
-			ArrayList<InserterDeleter<T>> arrayList = new ArrayList<>();
-			for (int i = 0; i < 1; ++i)
-				arrayList.add(new InserterDeleter<T>(eng, new Record<>(0, 0)));
-			// arrayList.add(new Lookuper<T>(eng, i));
-			// arrayList.add(new Inserter<T>(eng, new Record<>(i, i)));
+			// ArrayList<LookupWait<T>> arrayList = new ArrayList<>();
+			ArrayList<Deleter<T>> arrayList = new ArrayList<>();
+			// ArrayList<InserterDeleter<T>> arrayList = new ArrayList<>();
+			for (int i = 0; i < 100; ++i)
+				// arrayList.add(new InsertAbort<T>(eng, new Record<>(0, 0)));
+				// arrayList.add(new Inserter<T>(eng, new Record<>(i, i)));
+				// arrayList.add(new Lookuper<T>(eng, i));
+			// arrayList.add(new LookupWait<T>(eng, i));
+				arrayList.add(new Deleter<T>(eng, i));
+			// arrayList.add(new InserterDeleter<T>(eng, new Record<>(0,
+			// 0)));
 			List<Future<T>> invokeAll = exec.invokeAll(arrayList, 1000000,
 				TimeUnit.MILLISECONDS);
 			for (Future<T> future : invokeAll) {
